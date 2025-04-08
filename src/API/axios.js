40991050133e465bebe5c5d0295fdb11bd4c46a8
Chuyedi <130406303+Chuyedi@users.noreply.github.com>
2025-04-07 21:32:41 -0400

    Frontend

---
+++ src/API/axios.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const axiosBase = axios.create({
+    baseURL:'https://evangadi-forum-backend-4wku.onrender.com/api'
+})
+
+
+// axiosBase.interceptors.request.use(
+//     (config) => {
+//       const token = localStorage.getItem("token");
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//     },
+//     (error) => Promise.reject(error)
+//   );
+  
+//   // Add response interceptor
+//   axiosBase.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//       console.error("API Error:", error.response?.data);
+//       return Promise.reject(error);
+//     }
+//   );
+
+export default axiosBase
+
